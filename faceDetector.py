f544952a5d2e2a74bc2bdb0a6259f178109a79a5
BitByte <paulnikhil123@gmail.com>
2020-03-03 22:55:00 +0530

    resized window added cv2.residedWindow

--- faceDetector.py
+++ faceDetector.py
@@ -1,7 +1,7 @@
 import cv2
-face_cascade1 = cv2.CascadeClassifier('./prof.xml')
+face_cascade1 = cv2.CascadeClassifier('./xmls/prof.xml')
 face_cascade2 = cv2.CascadeClassifier('./xmls/face_alt2.xml')
-img = cv2.imread('./imgs/a.jpg')
+img = cv2.imread('./imgs/group.jpeg')
 # print(img.shape)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 detected_faces1 = face_cascade1.detectMultiScale(gray)
@@ -23,6 +23,9 @@ for (column, row, width, height) in detected_faces2:
         (255, 0, 0),
         2
     )
-cv2.imshow("image",img)
+cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+cv2.imshow("Image", img)
+cv2.resizeWindow('Image', 600, 600)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
